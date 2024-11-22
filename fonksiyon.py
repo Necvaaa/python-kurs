dff02e31f014ad3e4ab062c14e6a00194fdf4e16
necva.yerlikaya <necva.alks@gmail.com>
2024-11-22 21:57:13 +0300

    yeni değişiklik

--- fonksiyon.py
+++ fonksiyon.py
@@ -1,2 +1,4 @@
-def my_func(a,b)
+def my_func(a,b):
 print (a,b)
+def my_func2(x,y):
+return x%y
--- fonksiyon2.py
+++ fonksiyon2.py
@@ -1,5 +1,6 @@
-def topla(a,b):
-return a+b
+def carp(a,b):
+return a*b
 
 def cikar(a,b):
 return a-b
+
